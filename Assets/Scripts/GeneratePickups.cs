003d11fb7e51475d9c4f146383bb55684cab0a82
William Huang <william2958@gmail.com>
2019-02-12 10:32:08 -0500

    Project Added

---
+++ Assets/Scripts/GeneratePickups.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePickups : MonoBehaviour {
+
+    // C#
+    // Instantiates a prefab in a circle
+
+    public GameObject prefab;
+    public int numObjects = 12;
+    public float radius = 4.8f;
+    private Vector3 origin = new Vector3(0, 0.5f, 0);
+    void Start()
+    {
+
+        for (int i = 0; i < numObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numObjects;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+            GameObject pickup = Instantiate(prefab, origin + pos, Quaternion.identity);
+
+            switch(i % 3 + 1)
+            {
+                case 1:
+                  pickup.tag = "cyanCube";
+                  pickup.GetComponent<Renderer>().material.color = Color.cyan;
+                  break;
+
+                case 2:
+                pickup.GetComponent<Renderer>().material.color = Color.red;
+                pickup.tag = "redCube";
+                break;
+
+                case 3:
+                pickup.GetComponent<Renderer>().material.color = Color.magenta;
+                pickup.tag = "magentaCube";
+                break;
+
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    private int scoreCount;
+    public Text countText;
+    public Text winText;
+
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        scoreCount = 0;
+        setCountText();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other) {
+       if (other.gameObject.CompareTag("cyanCube")) {
+            other.gameObject.SetActive(false);
+            scoreCount = scoreCount + 1;
+            setCountText();
+        } else if (other.gameObject.CompareTag("redCube")) {
+            other.gameObject.SetActive(false);
+      scoreCount = scoreCount + 2;
+            setCountText();
+        } else if (other.gameObject.CompareTag("magentaCube")) {
+            other.gameObject.SetActive(false);
+      scoreCount = scoreCount + 3;
+            setCountText();
+        } 
+  }
+
+    void setCountText()
+    {
+        countText.text = "Score: " + scoreCount.ToString();
+
+    }
+}
